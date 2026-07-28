dc5fc44f276075aa80a4e916c4b13c5287fa7aab
Cris Rivera <crismrivera.07@gmail.com>
2026-07-28 16:01:30 -0500

    Fix filename extension stripping using split() instead of fixed-length slices.

--- functions.py
+++ functions.py
@@ -15,11 +15,13 @@ def generate_pdf_from_text(file):
     with open(file) as f:
         text = f.read()
 
+    filename_break = file.split(".")
+    filename = filename_break[0]
     pdf = PDF()
     pdf.add_page()
     pdf.set_font('helvetica', style= '', size=10)
     pdf.multi_cell(0, 20, text, align="C")
-    pdf.output(f"{(file[:9])}.pdf")
+    pdf.output(f"{filename}.pdf")
     return file
 
 print(generate_pdf_from_text("text_file.txt"))
