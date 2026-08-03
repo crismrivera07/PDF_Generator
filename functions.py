7e10cf004d2a2ce60cc116654e089ea678cf64ca
Cris Rivera <crismrivera.07@gmail.com>
2026-08-03 16:13:28 -0500

    Add error handling & fix return values in PDF generator txt functions.

--- functions.py
+++ functions.py
@@ -12,21 +12,25 @@ class PDF(FPDF):
         self.set_font('helvetica', style= 'B', size=10)
         self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
 
-"""def generate_pdf_from_text(file):
-    with open(file) as f:
-        text = f.read()
+def generate_pdf_from_text(file, newfilename):
+    try:
+        with open(file) as f:
+            text = f.read()
+    except FileNotFoundError:
+        print("Oops, that file does not exist, please try again please")
+        return 
 
     filename_break = file.split(".")
     filename = filename_break[0]
-
     pdf = PDF()
     pdf.add_page()
     pdf.set_font('helvetica', style= '', size=10)
     pdf.multi_cell(0, 20, text, align="C")
-    pdf.output(f"{filename}.pdf")
+    pdf.output(f"{newfilename}.pdf")
+    print("\nHere is your text file, generated as a PDF file.")
+    return True 
 
-
-def generate_pdf_from_csv(csv_file):
+def generate_pdf_from_csv(csv_file, newfilename):
     with open(csv_file, newline='', encoding='utf-8-sig', mode='r') as f:
         data = list(csv.reader(f, delimiter=','))
 
@@ -38,14 +42,10 @@ def generate_pdf_from_csv(csv_file):
             row = table.row()
             for datum in data_row:
                 row.cell(datum)
-    pdf.output(f"{csv_file}.pdf")"""
-
-
+    pdf.output(f"{newfilename}.pdf")
+    print("\nHere is your csv file, generated as a PDF file.")
 
 def strip_extension(file):
     broken_up_filename = file.split('.')
-    csv_extension = broken_up_filename.pop(-1)
+    extension = broken_up_filename.pop(-1)
     return ".".join(broken_up_filename)
-
-print(strip_extension("text_file.txt"))
-print(strip_extension("topics.csv"))
